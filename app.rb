cb4247633153ed5a87789cb04a10e4aee113e1e1
Flatiron School <andrewsprague@outlook.com>
2018-09-18 15:52:36 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,15 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    og = params[:string]
+    @new_string = og.reverse
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
-
+    @friends = ['Leah Severin', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+    erb :friends
   end
 end
